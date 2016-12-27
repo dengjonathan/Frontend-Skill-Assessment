b3164db10a49043a0a1291005004d41b64aeaf79
jonathan.deng <dengjonathan@gmail.com>
2016-12-27 14:28:48 -0500

    feat (store): init redux store

---
+++ src/store/store.js
@@ -0,0 +1,11 @@
+import {createStore} from 'redux';
+
+import rootReducer from '../reducers/root'
+
+const store = createStore(rootReducer);
+
+export const getState = () => store.getState();
+
+export const dispatch = action => store.dispatch(action);
+
+export default store;
